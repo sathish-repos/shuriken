16bf600cc5dd2d599bae55a9867c73aa022bdc16
sathish <me.sathish.git@gmail.com>
2024-07-09 19:08:28 +0530

    create villages comp

---
+++ src/pages/collections/villages/villages.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nu-villages',
+  standalone: true,
+  imports: [],
+  templateUrl: './villages.component.html',
+  styleUrl: './villages.component.scss'
+})
+export class VillagesComponent {
+
+}
