b9f967d0ee02de517d23a547c02d72c6e948becc
sathish <me.sathish.git@gmail.com>
2024-07-09 21:52:50 +0530

    character comp

--- src/app/pages/collections/components/characters/characters.component.ts
+++ src/app/pages/collections/components/characters/characters.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Character } from '../../models';
 
 @Component({
   selector: 'nu-characters',
   standalone: true,
   imports: [],
   templateUrl: './characters.component.html',
-  styleUrl: './characters.component.scss'
+  styleUrl: './characters.component.scss',
 })
 export class CharactersComponent {
-
+  content = input<Character>();
 }
---
+++ src/app/pages/shared/components/character/character.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nu-character',
+  standalone: true,
+  imports: [],
+  templateUrl: './character.component.html',
+  styleUrl: './character.component.scss'
+})
+export class CharacterComponent {
+
+}
