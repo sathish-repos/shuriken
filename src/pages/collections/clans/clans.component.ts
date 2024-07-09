28e75312ab09df8555e1abe10e96de9008b5ea73
sathish <me.sathish.git@gmail.com>
2024-07-09 19:07:40 +0530

    create clans comp

---
+++ src/pages/collections/clans/clans.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nu-clans',
+  standalone: true,
+  imports: [],
+  templateUrl: './clans.component.html',
+  styleUrl: './clans.component.scss'
+})
+export class ClansComponent {
+
+}
