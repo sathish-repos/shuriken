15a3a708fd442313236ee7702f5c6d49bf56e5b2
sathish <me.sathish.git@gmail.com>
2024-07-09 19:12:48 +0530

    create Akatsuki comp

---
+++ src/pages/collections/akatsuki/akatsuki.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nu-akatsuki',
+  standalone: true,
+  imports: [],
+  templateUrl: './akatsuki.component.html',
+  styleUrl: './akatsuki.component.scss'
+})
+export class AkatsukiComponent {
+
+}
