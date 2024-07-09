1e421c27108f9665f7030f42328de2b57525d55a
sathish <me.sathish.git@gmail.com>
2024-07-09 21:10:27 +0530

    add http client to config

--- src/app/pages/collections/services/collection.service.ts
+++ src/app/pages/collections/services/collection.service.ts
@@ -1,4 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { Character, Characters } from '../models';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
-export class CollectionsService {}
+export class CollectionsService {
+  http = inject(HttpClient);
+  protected baseUrl: string = 'https://dattebayo-api.onrender.com';
+
+  getCharacters(id?: string): Observable<Character> | Observable<Characters> {
+    if (id) {
+      return this.http.get<Character>(`${this.baseUrl}/characters/${id}`);
+    }
+    return this.http.get<Characters>(`${this.baseUrl}/characters`);
+  }
+}
