86aa9df0e6d7ba249e276bf952c0357501c35d1c
sathish <me.sathish.git@gmail.com>
2024-07-09 19:09:29 +0530

    create Kekkei-Genkai comp

---
+++ src/pages/collections/kekkei-genkai/kekkei-genkai.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nu-kekkei-genkai',
+  standalone: true,
+  imports: [],
+  templateUrl: './kekkei-genkai.component.html',
+  styleUrl: './kekkei-genkai.component.scss'
+})
+export class KekkeiGenkaiComponent {
+
+}
