2e5f3986548930a99ce5ff5247f778ecbace4a1c
sathish <me.sathish.git@gmail.com>
2024-07-09 19:13:28 +0530

    create Kara comp

---
+++ src/pages/collections/kara/kara.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nu-kara',
+  standalone: true,
+  imports: [],
+  templateUrl: './kara.component.html',
+  styleUrl: './kara.component.scss'
+})
+export class KaraComponent {
+
+}
