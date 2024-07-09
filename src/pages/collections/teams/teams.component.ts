a0fd4032404d370ac038142ec10fbd53a1943bf8
sathish <me.sathish.git@gmail.com>
2024-07-09 19:12:14 +0530

    create Teams comp

---
+++ src/pages/collections/teams/teams.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nu-teams',
+  standalone: true,
+  imports: [],
+  templateUrl: './teams.component.html',
+  styleUrl: './teams.component.scss'
+})
+export class TeamsComponent {
+
+}
