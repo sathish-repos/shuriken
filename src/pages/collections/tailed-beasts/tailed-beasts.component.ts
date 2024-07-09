aba716719e180371721a0df2b78a06920f18bb39
sathish <me.sathish.git@gmail.com>
2024-07-09 19:11:18 +0530

    create Tailed-Beasts comp

---
+++ src/pages/collections/tailed-beasts/tailed-beasts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nu-tailed-beasts',
+  standalone: true,
+  imports: [],
+  templateUrl: './tailed-beasts.component.html',
+  styleUrl: './tailed-beasts.component.scss'
+})
+export class TailedBeastsComponent {
+
+}
