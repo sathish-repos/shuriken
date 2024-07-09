fbde62a52be02900398b4391bb4712e79254fcf2
sathish <me.sathish.git@gmail.com>
2024-07-09 23:42:15 +0530

    add feed component

---
+++ src/app/pages/shared/components/feed/feed.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nu-feed',
+  standalone: true,
+  imports: [],
+  templateUrl: './feed.component.html',
+  styleUrl: './feed.component.scss'
+})
+export class FeedComponent {
+
+}
