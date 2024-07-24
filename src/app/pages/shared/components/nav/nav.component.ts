f847b36ea51b3e4c8a9cb2f017c95ec576d40c3e
sathish <me.sathish.git@gmail.com>
2024-07-24 23:17:03 +0530

    add router link active

--- src/app/pages/shared/components/nav/nav.component.ts
+++ src/app/pages/shared/components/nav/nav.component.ts
@@ -1,10 +1,10 @@
 import { Component, input } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'nu-nav',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss',
 })
