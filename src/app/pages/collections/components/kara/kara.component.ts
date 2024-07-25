e4410467f36a3e5f8e5b26e181322d8fac9a5fb2
sathish <me.sathish.git@gmail.com>
2024-07-25 11:10:05 +0530

    add karas profiles

--- src/app/pages/collections/components/kara/kara.component.ts
+++ src/app/pages/collections/components/kara/kara.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Karas } from '../../models';
+import { Observable } from 'rxjs';
+import { CollectionsService } from '../../services/collection.service';
+import { AsyncPipe } from '@angular/common';
+import { CharacterGridComponent } from '../../../shared/components/character-grid/character-grid.component';
 
 @Component({
   selector: 'nu-kara',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, CharacterGridComponent],
   templateUrl: './kara.component.html',
   styleUrl: './kara.component.scss',
 })
-export default class KaraComponent {}
+export default class KaraComponent {
+  collectionsService = inject(CollectionsService);
+  content$: Observable<Karas> = this.collectionsService.getKaras();
+}
--- src/app/pages/collections/services/collection.service.ts
+++ src/app/pages/collections/services/collection.service.ts
@@ -1,6 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Akatsukis, Character, Characters, Clans, Group } from '../models';
+import {
+  Akatsukis,
+  Character,
+  Characters,
+  Clans,
+  Group,
+  Karas,
+} from '../models';
 import { Observable } from 'rxjs';
 import { GroupTypeAction } from '../../shared/enums/group-type.enum';
 
@@ -9,23 +16,31 @@ export class CollectionsService {
   http = inject(HttpClient);
   protected baseUrl: string = 'https://dattebayo-api.onrender.com';
 
-  getCharacters(): Observable<Characters> {
-    return this.http.get<Characters>(`${this.baseUrl}/characters`);
-  }
-
   getCharacter(id: string): Observable<Character> {
     return this.http.get<Character>(`${this.baseUrl}/characters/${id}`);
-  }
-
-  getAkatsukis() {
-    return this.http.get<Akatsukis>(`${this.baseUrl}/akatsuki`);
   }
 
   getGroup(group: GroupTypeAction, id: string) {
     return this.http.get<Group>(`${this.baseUrl}/${group}/${id}`);
   }
 
+  getCharacters(): Observable<Characters> {
+    return this.http.get<Characters>(`${this.baseUrl}/characters`);
+  }
+
+  getAkatsukis() {
+    return this.http.get<Akatsukis>(`${this.baseUrl}/akatsuki`);
+  }
+
   getClans() {
     return this.http.get<Clans>(`${this.baseUrl}/clans`);
+  }
+
+  getKaras() {
+    return this.http.get<Karas>(`${this.baseUrl}/kara`);
+  }
+
+  getKekkeiGenkais() {
+    return this.http.get<Karas>(`${this.baseUrl}/kekkei-genkai`);
   }
 }
